53ec5e58aacb3b58192a2fca950fe4548bcb04a5
sd <leowest1921@gmail.com>
2023-06-05 15:07:39 +0800

    add

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,21 +1,17 @@
 import assert from "assert";
 beforeEach(async function () {
-   await wait(1000);
+  await wait(1000);
 });
 
-describe("多进程比较取值", function () {
-   it("比较取值是否相等", async function () {
-      let vv = { name: "li lei" };
-      console.info(">>>vv2", vv);
-      assert.deepStrictEqual(vv.name, "li lei");
-   });
-   it("查询", async function () {
-      let list = [];
-      console.log("list ==", list);
-   });
+describe("aa", function () {
+  it("test", async function () {
+    let vv = { name: "li lei" };
+    console.info(">>>vv2", vv);
+    assert.deepStrictEqual(vv.name, "li lei");
+  });
 });
 async function wait(ttl) {
-   return new Promise((resolve) => {
-      setTimeout(() => resolve(undefined), ttl);
-   });
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(undefined), ttl);
+  });
 }
